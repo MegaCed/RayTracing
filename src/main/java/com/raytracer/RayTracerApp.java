bf7fe61e8023d313f519a180ec847f390ecc38b2
SuperCed <class.cast.exception@gmail.com>
2021-11-22 17:36:11 +0100

    Project configuration

---
+++ src/main/java/com/raytracer/RayTracerApp.java
@@ -0,0 +1,12 @@
+package com.raytracer;
+
+public class RayTracerApp {
+
+	public static void main(String[] args) {
+		System.out.println("Starting App...");
+
+		
+		System.out.println("Done!");
+	}
+
+}
